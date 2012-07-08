7089e77c36acde44e6e8e226136cd93b5502c389
Jörg Jünger <joerg@jj-projects.de>
2012-07-08 10:51:16 +0200

    add licensing

--- src/de/jjprojects/cacheman/cacheman.java
+++ src/de/jjprojects/cacheman/cacheman.java
@@ -4,8 +4,6 @@
 package de.jjprojects.cacheman;
 
 /**
- * changes Groundworks cache gpx files;
- * adds comment field including Terrain, Difficulty and alike to get this information into an etrex Vista
  *  
  * @author Joerg Juenger, JJ-Projects
  */
@@ -31,8 +29,25 @@ import org.xml.sax.helpers.XMLReaderFactory;
 
 
 /**
- * @author joerg
- *
+ changes Groundworks cache gpx files;<BR>
+ adds comment field including Terrain, Difficulty and alike to get this information into an etrex Vista
+ 
+ @author Copyright (C) 2012  JJ-Projects Joerg Juenger <BR>
+  
+<pre>
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ </pre>
  */
 public class cacheman extends DefaultHandler
 {
